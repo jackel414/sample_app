2fb673449f0f13bf2242f18a34d67d84b05d5c5d
Zachary Mays <zackmays@gmail.com>
2014-07-08 16:01:42 -0400

    Add index page and styles

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Flying Vines Temp Page</title>
+		<link rel="stylesheet" href="styles.css">
+	</head>
+
+	<body>
+		<h1>Temporary Splash Page</h1>
+		<h2>The time is now <?php echo date("D M d, Y G:i a"); ?><h2>
+	</body>
+</html>
