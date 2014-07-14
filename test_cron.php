58ed444494e08c56b7b5537380c142415c9a4bd3
Zachary Mays <zackmays@gmail.com>
2014-07-14 13:52:20 -0400

    Make cron redirect if GET request

--- test_cron.php
+++ test_cron.php
@@ -1,10 +1,15 @@
 <?php
-	//send test email
-	$date_time = date("m/d - Ga");
-	$to = "zack@mays.io";
-	$subject = "Cron Job Test: $date_time ";
-	$message = "This is an email to test setting up a cron job.";
-	$from = "zack@flyingvines.com";
-	$headers = "From:" . $from;
-	mail($to,$subject,$message,$headers);
+  //check for POST request
+  if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    echo "<script> window.location.replace('index.php') </script>";
+  } else {
+  	//send test email
+  	$date_time = date("m/d - Ga");
+  	$to = "zack@mays.io";
+  	$subject = "Cron Job Test: $date_time ";
+  	$message = "This is an email to test setting up a cron job.";
+  	$from = "zack@flyingvines.com";
+  	$headers = "From:" . $from;
+  	mail($to,$subject,$message,$headers);
+  }
 ?>
